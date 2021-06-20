044743320e38394dac4b0064fc627bd34decba13
RAHUL DUTTA <RAHUL DUTTA@DESKTOP-FU9B0PE>
2021-06-20 11:06:04 +0530

    second commit

--- JavaDemo/src/com/test/Calculator.java
+++ JavaDemo/src/com/test/Calculator.java
@@ -8,6 +8,7 @@ public class Calculator
 	int sum;
 	int sub;
 	int multiply;
+	int any;
 	float div;
 	
    public void add( )
@@ -35,22 +36,30 @@ public class Calculator
 	   div=a1/a2;
  }
  
+ public void anonymous()
+ {
+	 any=(a1*a2)+a1;
+ }
+ 
 	public static void main(String[] args)
 	{
 		
         Calculator calc = new Calculator();
-        calc.a1=6;
+        calc.a1=7;
         calc.a2=3;
         
         calc.add();
         calc.sub();
         calc.multiply();
         calc.divide();
+        calc.anonymous();
         
-        System.out.println( "The sum of a1 and a2:" +calc.sum);
-        System.out.println(  calc.sub);
-        System.out.println(  calc.multiply);
-        System.out.println(  calc.div);
+        System.out.println( "The Addition of a1 and a2:" +calc.sum);
+        System.out.println( "The subtraction of a1 and a2:"  +calc.sub);
+        System.out.println(  "The Multiplication of a1 and a2:" +calc.multiply);
+        System.out.println(  "The Divison of a1 and a2:" +calc.div);
+        System.out.println(  "The anonymous of a1 and a2:" +calc.any);
+        
         
 	
 	
