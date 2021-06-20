b6463648bdd81933de2abed35782c87b064cfc86
rahul9596 <86179211+rahul9596@users.noreply.github.com>
2021-06-20 12:21:40 +0530

    Update Abc.java

--- JavaDemo/src/com/test/Abc.java
+++ JavaDemo/src/com/test/Abc.java
@@ -4,7 +4,7 @@ public class Abc {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World and Thank You");
+		System.out.println("Hello World and Thank You guys");
 
 	}
 
